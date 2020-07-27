4790e6de69cc204ad451bb33968c1e98dbf58901
Vitor <vitorbarbosa1998@hotmail.com>
2020-07-27 18:49:31 +0100

    feat: added ButtonLink component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "../../assets/img/vitorflix.png";
 import "./Menu.css";
+import ButtonLink from "./components/ButtonLink";
 
 function Menu() {
   return (
@@ -8,6 +9,9 @@ function Menu() {
       <a href="/">
         <img className="Logo" src={Logo} alt="VitorFlix" />
       </a>
+      <ButtonLink className="ButtonLink" href="/">
+        New Video
+      </ButtonLink>
     </nav>
   );
 }
