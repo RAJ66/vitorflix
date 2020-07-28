6179797c3a7b6863fc0971284f04b5e16d4a584b
Vitor <vitorbarbosa1998@hotmail.com>
2020-07-28 11:21:59 +0100

    feat:added router and page 404

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route component={() => <h1>Error 404</h1>} />
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById("root")
 );
