3c1b9ad58e4f5a86d0de149a1708d1a966d3e9fe
Vitor <vitorbarbosa1998@hotmail.com>
2020-08-01 15:40:12 +0100

    feat: complete home page 😀

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Menu from "../../components/Menu";
-//import dadosIniciais from "../../data/dados_iniciais.json";
 import BannerMain from "../../components/BannerMain";
 import Carousel from "../../components/Carousel";
 import Footer from "../../components/Footer";
@@ -24,23 +23,38 @@ function Home() {
     , [])
 
   return (
-    <PageDefault >
+    <PageDefault paddingAll={0} >
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área"}
-      />
 
-      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+      {dadosIniciais.length === 0 && (<div>Loading ...</div>)}
 
-      <Carousel category={dadosIniciais.categorias[1]} />
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription={dadosIniciais[0].videos[0].description}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
 
-      <Carousel category={dadosIniciais.categorias[2]} />
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
 
-      <Carousel category={dadosIniciais.categorias[3]} />
 
-      <Carousel category={dadosIniciais.categorias[4]} />
+
 
     </PageDefault>
   );
